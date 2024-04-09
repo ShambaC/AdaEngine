6466cab8980565e92dc6cc487896b6d97dc56d48
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-04-09 17:22:19 +0530

    Added texture batching
    known issue of wrong colors in textures

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -3,15 +3,21 @@
 
 layout (location=0) in vec3 aPos;
 layout (location=1) in vec4 aColor;
+layout (location=2) in vec2 aTexCoords;
+layout (location=3) in float aTexID;
 
 uniform mat4 uProjection;
 uniform mat4 uView;
 
 out vec4 fColor;
+out vec2 fTexCoords;
+out float fTexID;
 
 void main()
 {
     fColor = aColor;
+    fTexCoords = aTexCoords;
+    fTexID = aTexID;
     gl_Position = uProjection * uView * vec4(aPos, 1.0);
 }
 
@@ -19,10 +25,21 @@ void main()
 #version 330 core
 
 in vec4 fColor;
+in vec2 fTexCoords;
+in float fTexID;
+
+uniform sampler2D uTextures[8];
 
 out vec4 color;
 
 void main()
 {
-    color = fColor;
+    if (fTexID > 0) {
+        int id = int(fTexID);
+        color = fColor * texture(uTextures[id], fTexCoords);
+        // color = vec4(fTexCoords, 0, 1);
+    }
+    else {
+        color = fColor;
+    }
 }
