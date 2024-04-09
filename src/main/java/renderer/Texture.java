d134d6c18ec429b9d7527596e01a3e4e8985734d
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-04-10 00:34:22 +0530

    Add spritesheet handling

--- src/main/java/renderer/Texture.java
+++ src/main/java/renderer/Texture.java
@@ -11,6 +11,8 @@ import static org.lwjgl.stb.STBImage.*;
 public class Texture {
     private String filepath;
     private int texID;
+    private int width, height;
+
     public Texture(String filepath) {
         this.filepath = filepath;
 
@@ -35,6 +37,8 @@ public class Texture {
         ByteBuffer image = stbi_load(filepath, width, height, channels, 0);
 
         if (image != null) {
+            this.width = width.get(0);
+            this.height = height.get(0);
             if(channels.get(0) == 4)
                 glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width.get(0), height.get(0), 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
             else if(channels.get(0) == 3)
@@ -55,5 +59,13 @@ public class Texture {
 
     public void unbind() {
         glBindTexture(GL_TEXTURE_2D, 0);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
     }
 }
