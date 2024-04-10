36920643d4c150b92c016250d0e34f52275b5850
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-04-10 16:57:41 +0530

    Add sprite animation using dirty flags

--- src/main/java/Ada/LevelEditorScene.java
+++ src/main/java/Ada/LevelEditorScene.java
@@ -8,6 +8,8 @@ import org.joml.Vector4f;
 import util.AssetPool;
 
 public class LevelEditorScene extends Scene {
+    private GameObject obj1;
+    private Spritesheet sprites;
 
     public  LevelEditorScene() {
 
@@ -19,9 +21,9 @@ public class LevelEditorScene extends Scene {
 
         this.camera = new Camera(new Vector2f());
 
-        Spritesheet sprites = AssetPool.getSpritesheet("assets/images/spritesheet.png");
+        sprites = AssetPool.getSpritesheet("assets/images/spritesheet.png");
 
-        GameObject obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
+        obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
         obj1.addComponent(new SpriteRenderer(sprites.getSprite(0)));
         this.addGameObjectToScene(obj1);
 
@@ -38,8 +40,22 @@ public class LevelEditorScene extends Scene {
                         16, 16, 26, 0));
     }
 
+    private int spriteIndex = 0;
+    private float spriteFlipTime = 0.2f;
+    private float spriteFlipTimeLeft = 0.0f;
+
     @Override
     public void update(float dt) {
+        spriteFlipTimeLeft -= dt;
+        if (spriteFlipTimeLeft <= 0) {
+            spriteFlipTimeLeft = spriteFlipTime;
+            spriteIndex++;
+            if(spriteIndex > 4) {
+                spriteIndex = 0;
+            }
+            obj1.getComponent(SpriteRenderer.class).setSprite(sprites.getSprite(spriteIndex));
+        }
+
         for (GameObject go : this.gameObjects) {
             go.update(dt);
         }
--- src/main/java/components/SpriteRenderer.java
+++ src/main/java/components/SpriteRenderer.java
@@ -1,6 +1,7 @@
 package components;
 
 import Ada.Component;
+import Ada.Transform;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
 import renderer.Texture;
@@ -8,9 +9,10 @@ import renderer.Texture;
 public class SpriteRenderer extends Component {
 
     private Vector4f color;
-    private Vector2f[] texCoords;
-    private Texture texture;
     private Sprite sprite;
+
+    private Transform lastTransform;
+    private boolean isDirty = false;
 
     public SpriteRenderer(Vector4f color) {
         this.color = color;
@@ -23,12 +25,15 @@ public class SpriteRenderer extends Component {
     }
     @Override
     public void start() {
-
+        this.lastTransform = gameObject.transform.copy();
     }
 
     @Override
     public void update(float dt) {
-
+        if (!this.lastTransform.equals(this.gameObject.transform)) {
+            this.gameObject.transform.copy(lastTransform);
+            this.isDirty = true;
+        }
     }
 
     public Vector4f getColor() {
@@ -42,5 +47,25 @@ public class SpriteRenderer extends Component {
 
     public Vector2f[] getTexCoords() {
         return sprite.getTexCoords();
+    }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = sprite;
+        this.isDirty = true;
+    }
+
+    public void setColor(Vector4f color) {
+        if(!this.color.equals(color)) {
+            this.color.set(color);
+            this.isDirty = true;
+        }
+    }
+
+    public boolean isDirty() {
+        return this.isDirty;
+    }
+
+    public void setClean() {
+        this.isDirty = false;
     }
 }
